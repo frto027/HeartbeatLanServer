d2eaf6ecb1ff2757b37fab53b4a745d081281e8e
frto027 <q602706150@gmail.com>
2024-06-10 14:18:37 +0800

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "top.frto027.heartbeatlanserver"
         minSdk = 24
         targetSdk = 33
-        versionCode = 4
-        versionName = "1.4"
+        versionCode = 5
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
